13ae817bc92fd6c8356c84e790a4bf32ad2f4ebd
yadihaoku <yadihaoku@126.com>
2015-07-20 11:28:50 +0800

    BasePaser getUUID 已经可以使用

--- Etao_beacon/junit_test/com/example/etao_beacon/TestScanDataParser.java
+++ Etao_beacon/junit_test/com/example/etao_beacon/TestScanDataParser.java
@@ -7,16 +7,18 @@ import android.test.AndroidTestCase;
 public class TestScanDataParser extends AndroidTestCase {
 
 	private BasePaser mParser;
+	private byte [] bytes;
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		
-		String layout = "f:7-8=asdf"	;
+		String layout = "f:7-8=215,u:9-25"	;
 		mParser = new BasePaser(layout);
+		bytes = new byte[]{2, 1, 6, 26, -1, 76, 0, 2, 21, -3, -91, 6, -109, -92, -30, 79, -79, -81, -49, -58, -21, 7, 100, 120, 37, 39, 17, 91, 70, -59, 8, 9, 84, 101, 115, 116, 49, 0, 0, 21, 22, -64, -34, 2, 0, 100, -31, 6, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	}
 	
 	public void testParseUuid(){
-		System.out.println("asdfasd");
+		System.out.println(mParser.getUUID(bytes));
 	}
 
 
--- Etao_beacon/src/com/etao/ble/BasePaser.java
+++ Etao_beacon/src/com/etao/ble/BasePaser.java
@@ -1,5 +1,6 @@
 package com.etao.ble;
 
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,14 +9,15 @@ public class BasePaser implements IParser {
 	/**
 	 * 特征码 标示
 	 */
-	private static final Pattern FEATURE_PATTERN = Pattern.compile("f\\:(\\d)-(\\d)=(\\w+)");
+	private static final Pattern FEATURE_PATTERN = Pattern.compile("f:(\\d)-(\\d)\\=(\\w+)");
 
-	private static final Pattern UUID_PATTERN = Pattern.compile("u\\:(\\d)-(\\d)");
+	private static final Pattern UUID_PATTERN = Pattern.compile("u\\:(\\d+)\\-(\\d+)");
 
-	private static final Pattern MAJOR_PATTERN = Pattern.compile("m\\:(\\d)-(\\d)");
+	private static final Pattern MAJOR_PATTERN = Pattern.compile("m\\:(\\d+)-(\\d+)");
 
-	private static final Pattern MINOR_PATTERN = Pattern.compile("mi\\:(\\d)-(\\d)");
+	private static final Pattern MINOR_PATTERN = Pattern.compile("s\\:(\\d+)-(\\d+)");
 
+	
 	private int mFeatureStartIndex;
 	private int mFeatureEndIndex;
 	private int mUuidStartIndex;
@@ -24,10 +26,16 @@ public class BasePaser implements IParser {
 	private int mMajorEndIndex;
 	private int mMinorStartIndex;
 	private int mMinorEndIndex;
+	
+	/**
+	 * 沃联 vBeacon 特征码   215
+	 */
+	private int mDefaultFeature = 0x215;
 
 	public BasePaser(String pattern_layout) {
 		splitLayout(pattern_layout);
 	}
+
 
 	private void splitLayout(String layout) {
 		String patterns[] = layout.split(",");
@@ -38,6 +46,7 @@ public class BasePaser implements IParser {
 			while (matcher.find()) {
 				mFeatureStartIndex = Integer.parseInt(matcher.group(1));
 				mFeatureEndIndex = Integer.parseInt(matcher.group(2));
+				mDefaultFeature = Integer.parseInt(matcher.group(3));
 			}
 			
 			matcher = MAJOR_PATTERN.matcher(p);
@@ -46,12 +55,51 @@ public class BasePaser implements IParser {
 				mMajorEndIndex = Integer.parseInt(matcher.group(2));
 			}
 			
+			matcher = UUID_PATTERN.matcher(p);
+			while(matcher.find()){
+				mUuidStartIndex = Integer.parseInt(matcher.group(1));
+				mUuidEndIndex = Integer.parseInt(matcher.group(2));
+			}
+			
+			matcher = MINOR_PATTERN.matcher(p);
+			while(matcher.find()){
+				mMinorEndIndex = Integer.parseInt(matcher.group(1));
+				mMajorStartIndex = Integer.parseInt(matcher.group(2));
+			}
+			
 		}
 	}
 
+	/**
+	 * UUID 共 16 字节，128位。
+	 * 标准的 UUID 长度串
+	 * 8-4-4-4-12
+	 * 
+	 * 
+	 */
 	@Override
 	public String getUUID(byte[] scanBytes) {
-		return null;
+		StringBuilder uuids = new StringBuilder();
+		if(mUuidEndIndex < scanBytes.length ){
+			for(int i=mUuidStartIndex; i < mUuidEndIndex;i++){
+				uuids.append(String.format("%02x",  scanBytes[i] ));
+			}
+		}
+		String lastStr = uuids.toString();
+		if(lastStr.length() == 32){
+			StringBuilder uuid = new StringBuilder();
+			uuid.append(lastStr.substring(0, 8));
+			uuid.append("-");
+			uuid.append(lastStr.substring(8, 12));
+			uuid.append("-");
+			uuid.append(lastStr.substring(12, 16));
+			uuid.append("-");
+			uuid.append(lastStr.substring(16, 20));
+			uuid.append("-");
+			uuid.append(lastStr.substring(20));
+			return uuid.toString();
+		}
+		return lastStr;
 	}
 
 	@Override
